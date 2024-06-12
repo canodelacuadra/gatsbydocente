774a88ccb0aaf689b8bdf0e4a92888bb6d3257cb
canodelacuadra <canodelacuadra@gmail.com>
2024-06-12 15:46:43 +0200

    renderizo condicioanalmente el componente

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,27 +5,31 @@ import Layout from "../components/layout";
 import ContactoCollapse from "../components/contactocollapse";
 import TandemModal from "../components/tandemmodal";
 import TandemSlider from "../components/tandemslider";
-import { Button } from 'primereact/button';  
+import { Button } from 'primereact/button';
 import Alumno from "../components/alumno";
 import MiMapa from "../components/mimapa";
 
 const IndexPage = () => {
   const [latLng, setLatLng] = useState(null);
+  const componente = <div>
 
+    <MiMapa setLatLng={setLatLng} />
+    <div className="coordinates">
+      {latLng ? (
+        <p>
+          Latitud: {latLng.lat}, Longitud: {latLng.lng}
+        </p>
+      ) : (
+        <p>Haz clic en el mapa para obtener las coordenadas del punto seleccionado</p>
+      )}
+    </div>
+  </div>
+  // https://stackoverflow.com/questions/51304195/react-leaflet-with-gatsby
   return (
     <Layout>
-      <div>
-        <MiMapa setLatLng={setLatLng} />
-        <div className="coordinates">
-          {latLng ? (
-            <p>
-              Latitud: {latLng.lat}, Longitud: {latLng.lng}
-            </p>
-          ) : (
-            <p>Haz clic en el mapa para obtener las coordenadas del punto seleccionado</p>
-          )}
-        </div>
-      </div>
+      {typeof window !== 'undefined' && componente}
+
+
     </Layout>
   );
 }

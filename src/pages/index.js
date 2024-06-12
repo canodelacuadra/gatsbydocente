186a91b1590c738e39c9ff8ec053d671a976171f
canodelacuadra <canodelacuadra@gmail.com>
2024-06-12 13:45:28 +0200

    pequeños cambios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,34 @@
-import  React, {useState} from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-import Layout from "../components/layout"
-import ContactoCollapse from "../components/contactocollapse"
-import TandemModal from "../components/tandemmodal"
-import TandemSlider from "../components/tandemslider"
+import React, { useState } from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import Layout from "../components/layout";
+import ContactoCollapse from "../components/contactocollapse";
+import TandemModal from "../components/tandemmodal";
+import TandemSlider from "../components/tandemslider";
 import { Button } from 'primereact/button';  
-import Alumno from "../components/alumno"
-import MiMapa from "../components/mimapa"
+import Alumno from "../components/alumno";
+import MiMapa from "../components/mimapa";
 
 const IndexPage = () => {
- 
+  const [latLng, setLatLng] = useState(null);
 
   return (
-  <>
- <Layout>
-
-<MiMapa></MiMapa>
- </Layout>
-
-
-
-
-  </>
-  )
+    <Layout>
+      <div>
+        <MiMapa setLatLng={setLatLng} />
+        <div className="coordinates">
+          {latLng ? (
+            <p>
+              Latitud: {latLng.lat}, Longitud: {latLng.lng}
+            </p>
+          ) : (
+            <p>Haz clic en el mapa para obtener las coordenadas del punto seleccionado</p>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
+export default IndexPage;
 
-
-export default IndexPage
